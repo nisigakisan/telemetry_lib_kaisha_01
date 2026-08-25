2ef6a7c28bd029a41fca22aca8462adf9e1ca07a
nisigakisan <toshio22@mbr.nifty.com>
2026-08-25 20:47:45 +0900

    クラスinクラスを解決した。

--- zs01.py
+++ zs01.py
@@ -16,6 +16,10 @@
 # mission_controller.py
 
 import inspect
+import sys
+from pprint import pprint
+
+sys.stdout.reconfigure(encoding="utf-8")
 
 from core import djm
 from core import pcu
@@ -42,6 +46,11 @@ modules = [
 
 # 型の種類を調査
 set_value_type = set()
+list_basic_type = ["str","int","NoneType","float"]
+list_class = []
+class_vs_member_type = {}
+class_vs_member_name = {}
+
 
 # importしたモジュールに含まれるclassの名前を取得
 def list_classnames_in_module():
@@ -52,21 +61,35 @@ def list_classnames_in_module():
 
 
 def print_inst(inst):
-    name_module_and_class = f"{type(inst).__module__}.{type(inst).__name__}"
-    name_module_and_class = name_module_and_class.replace("core.", "")
+    name_module = f"{type(inst).__module__}"
+    name_class = f"{type(inst).__name__}"
+
+    list_class.append(name_class)
+    class_vs_member_type[name_class] = []
+    class_vs_member_name[name_class] = []
     # print(f"☆instのモジュール名・クラス名：{name_module_and_class}")
 
     # print("＝＝print_inst(inst)ここから＝＝")
     # print(inst)
     # print("＝＝print_inst(inst)ここまで＝＝")
+    # return
 
     # print("＝＝メンバー一覧ここから＝＝")
-    for name_member, value in vars(inst).items():
+    for member_name, value in vars(inst).items():
+        outstr = ""
         value_type = type(value).__name__
+        if isinstance(value, list):
+            value_type = type(value[0]).__name__
+
         set_value_type.add(value_type)
-        outstr = f"{name_module_and_class}, {name_member}, {value_type}"
+
+        outstr += f"{name_module}, {name_class}, {member_name}, {value_type}"
         if isinstance(value, list):
             outstr += f", {len(value)}"
+        else:
+            outstr += f", 1"
+        class_vs_member_type[name_class].append(value_type)
+        class_vs_member_name[name_class].append(member_name)
         print(outstr)
     # print("＝＝メンバー一覧ここまで＝＝")
 
@@ -114,8 +137,12 @@ def create_instance_and_inspect():
     inst = mission_controller.MissionController()
     print_inst(inst)
 
-# ここからmain
+#==== ここからmain ====
+
+# importしたモジュールに含まれるclassの名前を取得
 # list_classnames_in_module()
+
+# inst作って__str__呼ぶ
 create_instance_and_inspect()
 
 print()
@@ -123,3 +150,59 @@ print("＝＝値の型一覧ここから＝＝")
 for value_type in set_value_type:
     print(f"{value_type}")
 print("＝＝値の型一覧ここまで＝＝")
+
+print()
+print("＝＝クラス名のメンバーが基本型かクラスかここから＝＝")
+list_class_basic_only = []
+list_class_has_class = []
+for class_name in list_class:
+    print(class_name)
+
+    flag = True
+    for member_type_1 in class_vs_member_type[class_name]:
+        if member_type_1 not in list_basic_type:
+            print("\t", member_type_1)
+            flag = False
+
+    if flag:
+        print("基本型だけ")
+        list_class_basic_only.append(class_name)
+    else:
+        print("基本型だけじゃない")
+        list_class_has_class.append(class_name)
+print("＝＝クラス名のメンバーが基本型かクラスかここまで＝＝")
+
+print()
+print("＝＝クラス名のメンバーが基本型だけ ここから＝＝")
+for class_name in list_class_basic_only:
+    print(class_name)
+print("＝＝クラス名のメンバーが基本型だけ ここまで＝＝")
+
+print()
+print("＝＝クラス名のメンバーが基本型だけじゃない ここから＝＝")
+for class_name in list_class_has_class:
+    print(class_name)
+print("＝＝クラス名のメンバーが基本型だけじゃない ここまで＝＝")
+
+print()
+print("＝＝クラスinクラスの解決 ここから＝＝")
+for class_name in list_class_has_class:
+    print()
+    print("☆" + class_name)
+    for i, member_type_1 in enumerate(class_vs_member_type[class_name]):
+        member_name_1 = class_vs_member_name[class_name][i]
+
+        if member_type_1  in list_basic_type:
+            print(f"{member_name_1}, {member_type_1}")
+        else:
+            print(f"{member_name_1}, {member_type_1} を解決する")
+            if member_type_1 in ["GpsIndex", "list"]:
+                print(f"{member_type_1} は解決不能なので深堀しない")
+                continue
+
+            for j, member_type_2 in enumerate(class_vs_member_type[member_type_1]):
+                member_name_2 = class_vs_member_name[member_type_1][j]
+                print(f"{member_name_1}.{member_type_1}.{member_name_2}, {member_type_2}")
+                if member_type_2 not in list_basic_type:
+                    print("2階層目でも基本型じゃない：", member_type_2)
+print("クラスinクラスの解決 ここまで＝＝")
