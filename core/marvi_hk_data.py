aa811740e5af977cfa5a6e59c1436c4509fe20a9
nisigakisan <gdevtoshio11@gmail.com>
2026-08-28 09:10:38 +0900

    marvi_hk_data.pyを追加した。

---
+++ core/marvi_hk_data.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from telemetry_lib.core.djm import Djm
+from telemetry_lib.core.gas import Gas
+from telemetry_lib.core.gyro import CeIru, Miru
+from telemetry_lib.core.gps import Gps
+from telemetry_lib.core.mission_controller import MissionController
+from telemetry_lib.core.mission_obc import MissionObc
+from telemetry_lib.core.obc import Obc
+from telemetry_lib.core.pbi import Pbi
+from telemetry_lib.core.pcu import Pcu
+from telemetry_lib.core.pdcu import Pdcu
+from telemetry_lib.core.power_shot import PowerShot
+from telemetry_lib.core.rw import Rw
+from telemetry_lib.core.spaceware_micro import SpacewareMicro
+from telemetry_lib.core.stt import Stt
+from telemetry_lib.core.attitude_control import AttitudeControl
+
+
+class MarviHkData(object):
+    """衛星の全てのコンポーネントの情報(HKデータ)を保持するクラス。"""
+
+    DJM_NUM = 5
+    RW_NUM = 4
+    STT_NUM = 4
+
+    HK_SIZE = (
+        Obc.HK_SIZE
+        + AttitudeControl.HK_SIZE
+        + Pcu.HK_SIZE
+        + Pdcu.HK_SIZE
+        + MissionObc.HK_SIZE
+        + Djm.HK_SIZE * DJM_NUM
+        + CeIru.HK_SIZE
+        + Miru.HK_SIZE
+        + Rw.HK_SIZE * RW_NUM
+        + Stt.HK_SIZE * STT_NUM
+        + Gps.HK_SIZE
+        + Gas.HK_SIZE
+        + Pbi.HK_SIZE
+        + SpacewareMicro.HK_SIZE
+        + PowerShot.HK_SIZE
+        + MissionController.HK_SIZE
+    )
+
+    print("MARVI HK_SIZE: %d" % HK_SIZE)
+
+    def __init__(self):
+        super(MarviHkData, self).__init__()
+        self.obc = Obc()
+        self.djm = [Djm(djm_id) for djm_id in range(1, 1 + self.DJM_NUM)]
+        self.pcu = Pcu()
+        self.pdcu = Pdcu()
+        self.mission_obc = MissionObc()
+        self.rw = [Rw(rw_id) for rw_id in range(self.RW_NUM)]
+        self.ce_iru = CeIru()
+        self.m_iru = Miru()
+        self.stt = [Stt(stt_id) for stt_id in range(self.STT_NUM)]
+        self.gps = Gps()
+        self.gas = Gas()
+        self.pbi = Pbi()
+        self.spaceware_micro = SpacewareMicro()
+        self.att = AttitudeControl()
+        self.ps = PowerShot()
+        self.mission = MissionController()
+
